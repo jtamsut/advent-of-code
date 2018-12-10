260e858ec8d80b8882750790166fd85354221cfb
jtamsut <jtamsut1993@gmail.com>
2018-12-09 16:52:45 -0800

    progress on day 2

--- 2018/02/main.go
+++ 2018/02/main.go
@@ -1,24 +1,28 @@
 package main
 
-func letterFrequency(str string) map[string]int {
+import (
+	"fmt"
+
+	helpers "github.com/jtamsut/aochelpers"
+)
+
+func frequencies(str string) map[string]int {
 	m := make(map[string]int)
 	for _, c := range str {
-
+		g := string(c)
+		m[g]++
 	}
 	return m
 }
 
-// func processLines(n fileName)
-// func main() {
-// 	ids, err := helpers.BreakOnNewLines("./input.txt")
+func main() {
+	ids, err := helpers.BreakOnNewLines("./input.txt")
 
-// 	if err != nil {
-// 		helpers.WrapError(err)
-// 	}
+	if err != nil {
+		helpers.WrapError(err)
+	}
 
-// 	m := make(map[string]int)
-// 	for _, char := range ids {
-// 		m[char]
-// 	}
-// 	return m
-// }
+	for _, line := range ids {
+		fmt.Println(frequencies(line))
+	}
+}
