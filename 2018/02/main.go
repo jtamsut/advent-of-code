88efded8b06c5a9856d0b8848114d376ad24a1e9
jtamsut <jtamsut1993@gmail.com>
2018-12-09 17:06:53 -0800

    part i, day 2

--- 2018/02/main.go
+++ 2018/02/main.go
@@ -15,14 +15,39 @@ func frequencies(str string) map[string]int {
 	return m
 }
 
+// returns true, true if 2 and 3 occurrences
+func doubleOrTriple(j map[string]int) (bool, bool) {
+	i := false
+	o := false
+	for _, v := range j {
+		if v == 2 {
+			i = true
+		}
+
+		if v == 3 {
+			o = true
+		}
+	}
+	return i, o
+}
+
 func main() {
 	ids, err := helpers.BreakOnNewLines("./input.txt")
 
 	if err != nil {
 		helpers.WrapError(err)
 	}
-
+	twos := 0
+	threes := 0
 	for _, line := range ids {
-		fmt.Println(frequencies(line))
+		w, x := doubleOrTriple(frequencies(line))
+		if w == true {
+			twos += 1
+		}
+		if x == true {
+			threes += 1
+		}
 	}
+
+	fmt.Printf("Part 1: %d\n", twos*threes)
 }
