3015bb78a3fe6755f7e50bd3d3596bc718a39a82
jtamsut <jtamsut1993@gmail.com>
2018-12-15 18:51:33 -0800

    refactor line parsing

--- 2018/03/main.go
+++ 2018/03/main.go
@@ -1,9 +1,62 @@
-package main 
+package main
 
 import (
-  "fmt"
+	"fmt"
+
+	helpers "github.com/jtamsut/aochelpers"
 )
 
-func main() {
-  fmt.Println("It works")  
+type box struct {
+	topX, topY, width, height int
 }
+
+func parse(s string) []box {
+	lines, err := helpers.BreakOnNewLines(s)
+
+	if err != nil {
+		helpers.WrapError(err)
+	}
+
+	boxes := make([]box, len(lines))
+
+	for i, line := range lines {
+		var tmpI int
+		fmt.Sscanf(line, "#%d @ %d,%d: %dx%d", &tmpI, &boxes[i].topX, &boxes[i].topY, &boxes[i].width, &boxes[i].height)
+	}
+
+	return boxes
+
+}
+
+func main() {
+	fmt.Println(parse("input.txt"))
+}
+
+// func main() {
+// 	var boxes []Box
+
+// 	for _, x := range line {
+// 		g := strings.Split(x, "@")
+// 		j := strings.Split(g[1], ":")
+// 		i := strings.Split(j[0], ",")
+// 		b := strings.Split(j[1], "x")
+
+// 		tX, _ := strconv.Atoi(i[0])
+// 		tY, _ := strconv.Atoi(i[1])
+// 		wi, _ := strconv.Atoi(b[0])
+// 		he, _ := strconv.Atoi(b[1])
+
+// 		fmt.Println(tX)
+
+// 		z := Box{
+// 			topX:   tX,
+// 			topY:   tY,
+// 			width:  wi,
+// 			height: he,
+// 		}
+
+// 		boxes = append(boxes, z)
+// 	}
+
+// 	// fmt.Println(boxes)
+// }
