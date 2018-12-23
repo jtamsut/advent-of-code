44834dbe11fe07a64cb8bf2e240d847a3d3900b7
jtamsut <jtamsut1993@gmail.com>
2018-12-23 12:13:32 -0800

    compute overlap with duplicates

--- 2018/03/main.go
+++ 2018/03/main.go
@@ -1,48 +1,49 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	helpers "github.com/jtamsut/aochelpers"
-// )
+	helpers "github.com/jtamsut/aochelpers"
+	geometry "github.com/jtamsut/aochelpers/geometry"
+)
 
-// type box struct {
-// 	topX, topY, width, height int
-// }
+func parse(s string) []geometry.Box {
+	lines, err := helpers.BreakOnNewLines(s)
+	boxes := make([]geometry.Box, len(lines))
 
-// func parse(s string) []box {
-// 	lines, err := helpers.BreakOnNewLines(s)
-// 	boxes := make([]box, len(lines))
+	helpers.WrapError(err)
 
-// 	helpers.WrapError(err)
+	for i, line := range lines {
+		var tmpI int
+		fmt.Sscanf(line, "#%d @ %d,%d: %dx%d",
+			&tmpI,
+			&boxes[i].LeftX,
+			&boxes[i].TopY,
+			&boxes[i].Width,
+			&boxes[i].Height)
+	}
 
-// 	for i, line := range l ines {
-// 		var tmpI int
-// 		fmt.Sscanf(line, "#%d @ %d,%d: %dx%d",
-// 			&tmpI,
-// 			&boxes[i].topX,
-// 			&boxes[i].topY,
-// 			&boxes[i].width,
-// 			&boxes[i].height)
-// 	}
+	return boxes
 
-// 	return boxes
+}
 
-// }
+func getArea() {
+	boxes := parse("./input.txt")
+	var area int
+	for i, bx := range boxes {
+		for j, bt := range boxes {
+			if i != j {
+				o, m := geometry.DetermineOverlap(bx, bt)
 
-// // TODO: Write a consolidation algorithm that will take a slice of boxes and simplify them down to the smallest slice possible
+				if o {
+					area += geometry.Area(m)
+				}
+			}
+		}
+	}
+	fmt.Println(area)
+}
 
-// func mergeBoxes() {
-// 	// will return a slice of []box
-// }
-
-// func main() {
-// 	boxes := parse("input.txt")
-// 	for _, i := range boxes {
-// 		o, b := compareTwoPoints(i, i)
-// 		fmt.Println(i)
-// 		fmt.Println(o)
-// 		fmt.Println(b)
-// 		fmt.Println("---")
-// 	}
-// }
+func main() {
+	getArea()
+}
