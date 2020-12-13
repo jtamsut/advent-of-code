2be368acaed2abf54a9297e5ed00345a9b512600
Jonathan Tamsut <jon@x-claim.com>
2020-12-13 15:32:22 -0800

    complete day 9

--- 2020/09/main.rb
+++ 2020/09/main.rb
@@ -9,19 +9,70 @@ class SumFinder < FileLoader
   end
 
   def call
-    each_line do |line, line_number|
-      binding.pry
-      puts line
-    end
+    @numbers = []
+    each_line { @numbers << @current_line.to_i }
+    @target = incorrect_num
+
+    range_sum
   end
 
   private
 
+  def range_sum
+    @numbers.each_with_index do |_, idx|
+      final_idx = idx
+
+      while (sum(idx, final_idx) < @target) && (final_idx < @numbers.length)
+        final_idx += 1
+
+        if sum(idx, final_idx) == @target
+          range = @numbers[idx..final_idx]
+
+          puts range.min + range.max
+          return
+        end
+      end
+    end
+  end
+
+  def sum(start, finish)
+    @numbers[start..finish].inject(0, :+)
+  end
+
+  def incorrect_num
+    @numbers.each_with_index do |num, idx|
+      if idx > 24
+        prev = preamble(idx)
+
+        if !two_num_sum(prev, num)
+          return num
+        end
+      end
+    end
+
+  end
+
+  def preamble(idx)
+    start = idx - 25
+    end_pos = idx - 1
+
+    @numbers[start..end_pos]
+  end
+
   def two_num_sum(nums, num)
-    sums = {}
+    differences = {}
 
-  
+    nums.each_with_index do |curr_num, idx|
+      diff = num - curr_num
 
+      differences[diff] = idx if diff.positive?
+    end
+
+    nums.each_with_index do |curr_num, idx|
+      return true if !differences[curr_num].nil? && differences[curr_num] != idx
+    end
+
+    false
   end
 end
 
